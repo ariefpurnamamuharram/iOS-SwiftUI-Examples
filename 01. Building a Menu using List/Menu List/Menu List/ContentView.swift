b0980d2cc1c7e5115d7e81aa16bffb3681ff86a2
Arief Purnama Muharram <ariefpurnamamuharram@gmail.com>
2020-06-23 19:05:25 +0700

    Update menu list example project on 2020/06/23 19:05 WIB.

--- 01. Building a Menu using List/Menu List/Menu List/ContentView.swift
+++ 01. Building a Menu using List/Menu List/Menu List/ContentView.swift
@@ -8,14 +8,48 @@
 
 import SwiftUI
 
+struct MenuSection: Codable, Identifiable {
+    var id: UUID
+    var name: String
+    var items: [MenuItem]
+}
+
+struct MenuItem: Codable, Equatable, Identifiable {
+    var id: UUID
+    var name: String
+    var photoCredit: String
+    var price: Int
+    var restrictions: [String]
+    var description: String
+    
+    var mainImage: String {
+        name.replacingOccurrences(of: " ", with: "-").lowercased()
+    }
+    
+    var thumbnailImage: String {
+        "\(mainImage)-thumb"
+    }
+    
+    #if DEBUG
+    static let example = MenuItem(id: UUID(), name: "Mapple French Toast", photoCredit: "Joseph Gonzalez", price: 6, restrictions: ["G", "V"], description: "Sweet, fluffy, and served piping hot, our French toast is flown in fresh every day from Maple City, Canada, which is where all maple syrup in the world comes from. And if you believe that, we have some land to sell you...")
+    #endif
+}
+
 struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
         NavigationView {
             List {
-                Text("Hello World")
-                Text("Hello World")
-                Text("Hello World")
+                ForEach(menu) { section in
+                    Section(header: Text(section.name)) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
             }.navigationBarTitle("Menu")
+            .listStyle(GroupedListStyle())
         }
     }
 }
---
+++ 01. Building a Menu using List/README.md
@@ -0,0 +1,10 @@
+# Menu List
+This is an example project of how to show menu list in a fascinating way.
+
+![App screenshot](screenshot01.png)
+
+![App screenshot](screenshot02.png)
+
+## References
+- https://www.hackingwithswift.com/quick-start/swiftui/swiftui-tutorial-building-a-complete-project
+- https://www.hackingwithswift.com/quick-start/swiftui/building-a-menu-using-list
